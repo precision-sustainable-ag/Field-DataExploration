d718c654d3c2c75ad3f6fd5dad3ef7d4152553dc
MathewAaron <aaron.mathew0217@gmail.com>
2024-02-15 15:13:40 -0500

    updating run volume assessment

--- run_volume_assessment.sh
+++ run_volume_assessment.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python FIELD_REPORT.py general.task=wir_table_generator
+python FIELD_REPORT.py general.task=wir_table_generator &
+python FIELD_REPORT.py general.task=wir_blob_data_generator &
