0e8114d8426d1c6c90793c85db2a3af62cce96ab
mkutu <mattkutugata@gmail.com>
2024-06-17 12:48:01 -0400

    added plots of speceis for each state

--- scripts/report.py
+++ scripts/report.py
@@ -16,6 +16,7 @@ class BatchReport:
     """A class to generate reports for Field data visualization,
     focusing on different aspects of plant data.
     """
+
     def __init__(self, cfg) -> None:
         """Initialize the BatchReport with configuration and data loading."""
         self.cfg = cfg
@@ -55,8 +56,9 @@ class BatchReport:
     def write_duplicate_jpgs(self, df: pd.DataFrame) -> None:
         pass
 
-    def write_duplicate_raws(self, df:pd.DataFrame) -> None:
+    def write_duplicate_raws(self, df: pd.DataFrame) -> None:
         pass
+
     def write_missing_raws(self, df: pd.DataFrame) -> None:
         """Write information on missing raw images to a CSV file."""
         # By state, species, upload_time
@@ -73,8 +75,8 @@ class BatchReport:
             "Username",
             "HasMatchingJpgAndRaw",
         ]
-        df['UploadDateTimeUTC'] = pd.to_datetime(df['UploadDateTimeUTC'])
-        df['UploadDateUTC'] = df['UploadDateTimeUTC'].dt.date
+        df["UploadDateTimeUTC"] = pd.to_datetime(df["UploadDateTimeUTC"])
+        df["UploadDateUTC"] = df["UploadDateTimeUTC"].dt.date
         df = (
             df[df["HasMatchingJpgAndRaw"] == False][columns]
             # .drop_duplicates(subset="Name")
@@ -87,14 +89,32 @@ class BatchReport:
         """Creates a table of uploads from last 7 days by location in a csv format."""
 
         df = self.df.copy()
-        df['UploadDateTimeUTC'] = pd.to_datetime(df['UploadDateTimeUTC'])
-        df['UploadDateUTC'] = pd.to_datetime(df['UploadDateTimeUTC'].dt.date)
+        df["UploadDateTimeUTC"] = pd.to_datetime(df["UploadDateTimeUTC"])
+        df["UploadDateUTC"] = pd.to_datetime(df["UploadDateTimeUTC"].dt.date)
         current_date_time = pd.to_datetime(datetime.now().date())
-        seven_days_ago = pd.to_datetime(current_date_time - timedelta(days=7)) #calculate date 7 days ago
+        seven_days_ago = pd.to_datetime(
+            current_date_time - timedelta(days=7)
+        )  # calculate date 7 days ago
 
-        df_last_7_days = df[(df['UploadDateUTC'] >= seven_days_ago) & (df['UploadDateUTC'] <= current_date_time)].copy() #filter for last 7 days
-        df_last_7_days['IsDuplicated'] = df_last_7_days.duplicated('Name', keep=False)
-        grouped_df_last_7_days = df_last_7_days.groupby(["UsState","PlantType", "Species","Extension", "HasMatchingJpgAndRaw", "IsDuplicated"]).size().reset_index(name="count")
+        df_last_7_days = df[
+            (df["UploadDateUTC"] >= seven_days_ago)
+            & (df["UploadDateUTC"] <= current_date_time)
+        ].copy()  # filter for last 7 days
+        df_last_7_days["IsDuplicated"] = df_last_7_days.duplicated("Name", keep=False)
+        grouped_df_last_7_days = (
+            df_last_7_days.groupby(
+                [
+                    "UsState",
+                    "PlantType",
+                    "Species",
+                    "Extension",
+                    "HasMatchingJpgAndRaw",
+                    "IsDuplicated",
+                ]
+            )
+            .size()
+            .reset_index(name="count")
+        )
 
         grouped_df_last_7_days.to_csv(self.cfg.report.uploads_7days, index=False)
         log.info("Created table of uploads from last 7 days by location successfully.")
@@ -122,35 +142,35 @@ class BatchReport:
                 palette=self.planttype_palette,
                 ax=ax,
             )
-            
+
             ax.set_xticks(ax.get_xticks())
             ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
-            ax.set_title(
-                "Unique MasterRefIDs (samples) by State and Plant Type"
-            )
+            ax.set_title("Unique MasterRefIDs (samples) by State and Plant Type")
             # Adding a note under the title
-            ax.text(.0725, -.125, "$^{*}$HasMatchingJpgAndRaw = True", ha='center', fontsize=9, transform=ax.transAxes)
+            ax.text(
+                0.0725,
+                -0.125,
+                "$^{*}$HasMatchingJpgAndRaw = True",
+                ha="center",
+                fontsize=9,
+                transform=ax.transAxes,
+            )
             ax.set_ylabel("# MasterRefIDs (samples)")
             ax.set_xlabel("State Location")
             ax.legend(title="Plant Type")
             # Add labels to each bar
             for bar_container in bar_plot.containers:
-                ax.bar_label(bar_container, label_type='edge', padding=3, fontsize=7)
+                ax.bar_label(bar_container, label_type="edge", padding=3, fontsize=7)
 
-            
             fig.tight_layout()
-            save_path = (
-                f"{self.cfg.report.plots_all_years}/unique_masterrefids_by_state_and_planttype.png"
-            )
+            save_path = f"{self.cfg.report.plots_all_years}/unique_masterrefids_by_state_and_planttype.png"
             fig.savefig(save_path, dpi=300)
             log.info("Unique MasterRefIDs plot saved.")
 
     def plot_image_vs_raws_by_species(self):
         # Count the number of unique Images for each UsState and Extension
         unique_ids_count = (
-            self.df.groupby(["UsState", "Extension"])["Name"]
-            .nunique()
-            .reset_index()
+            self.df.groupby(["UsState", "Extension"])["Name"].nunique().reset_index()
         )
 
         # Plotting
@@ -166,9 +186,7 @@ class BatchReport:
             )
             ax.set_xticks(ax.get_xticks())
             ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
-            ax.set_title(
-                "Number of Images by State and by Image Extension"
-            )
+            ax.set_title("Number of Images by State and by Image Extension")
             ax.set_ylabel("Number of Images")
             ax.set_xlabel("State Location")
             ax.legend(title="Image Type")
@@ -178,7 +196,6 @@ class BatchReport:
             )
             fig.savefig(save_path, dpi=300)
             log.info("Jpg vs Raws plot saved.")
-        
 
     def plot_sample_species_distribution(self):
         data = self.df[self.df["HasMatchingJpgAndRaw"] == True]
@@ -207,7 +224,14 @@ class BatchReport:
             )
             ax.set_ylabel("Species")
             ax.set_xlabel("Number of Unique Samples")
-            ax.text(-.050, -.035, "$^{*}$HasMatchingJpgAndRaw = True", ha='center', fontsize=9, transform=ax.transAxes)
+            ax.text(
+                -0.050,
+                -0.035,
+                "$^{*}$HasMatchingJpgAndRaw = True",
+                ha="center",
+                fontsize=9,
+                transform=ax.transAxes,
+            )
             # g.tight_layout()
             ax.figure.suptitle("Samples by Species and Plant Type", fontsize=18)
             # Adding the number of samples at the end of each bar
@@ -222,12 +246,66 @@ class BatchReport:
                 )
             fig.tight_layout()
             # plt.subplots_adjust(top=0.93)
-            save_path = (
-                f"{self.cfg.report.plots_all_years}/unique_masterrefids_by_species_and_planttype.png"
-            )
+            save_path = f"{self.cfg.report.plots_all_years}/unique_masterrefids_by_species_and_planttype.png"
             fig.savefig(save_path, dpi=300)
             log.info("Species Distribution plot saved.")
-        
+
+    def plot_sample_species_state_distribution(self):
+        data = self.df[self.df["HasMatchingJpgAndRaw"]]
+        data = data[data["Extension"] == "arw"]
+        data["UsState"] = data["UsState"].replace("NC01", "NC")
+
+        samplecount_df = (
+            data.groupby(["UsState", "PlantType", "Species"])["MasterRefID"]
+            .nunique()
+            .reset_index(name="sample_count")
+            .sort_values(by="sample_count")
+        )
+
+        # Create a separate bar plot for each UsState
+        unique_states = samplecount_df["UsState"].unique()
+
+        for state in unique_states:
+            fig, ax = plt.subplots(figsize=(8, 6))
+            state_data = samplecount_df[samplecount_df["UsState"] == state]
+            sns.barplot(
+                data=state_data,
+                x="sample_count",
+                y="Species",
+                hue="PlantType",
+                palette=self.planttype_palette,
+                ax=ax,
+            )
+
+            # Adding the number of samples at the end of each bar
+            for p in ax.patches:
+                width = p.get_width()
+                ax.text(
+                    width + 1,
+                    p.get_y() + p.get_height() / 2,
+                    "{:1.0f}".format(width),
+                    ha="left",
+                    va="center",
+                )
+            ax.set_ylabel("Species")
+            ax.set_xlabel("Number of Unique Samples")
+            ax.text(
+                -0.050,
+                -0.085,
+                "$^{*}$HasMatchingJpgAndRaw = True",
+                ha="center",
+                fontsize=9,
+                transform=ax.transAxes,
+            )
+
+            ax.figure.suptitle(f"Samples by Species: {state}", fontsize=18)
+
+            fig.tight_layout()
+            # plt.subplots_adjust(top=0.93)
+            save_path = f"{self.cfg.report.plots_all_years}/unique_masterrefids_by_species_for_{state}.png"
+            fig.savefig(save_path, dpi=300)
+            log.info("Species Distribution plot saved.")
+
     def plot_num_samples_season(self):
         """Generate a bar plot showing the distribution of unique MasterRefIDs by plant type."""
 
@@ -246,26 +324,29 @@ class BatchReport:
             fig, ax = plt.subplots(figsize=(12, 6))
 
             bar_plot = sns.barplot(
-                data=unique_ids_count,
-                x="PlantType",
-                y="MasterRefID",
-                ax=ax,
-                width=0.25
+                data=unique_ids_count, x="PlantType", y="MasterRefID", ax=ax, width=0.25
             )
-            
+
             ax.set_xticks(ax.get_xticks())
             ax.set_xticklabels(ax.get_xticklabels(), rotation=0)
             ax.figure.suptitle("Samples by Species", fontsize=18)
 
-            # Adding a note under the title           
+            # Adding a note under the title
             annot_text = "$^{*}$HasMatchingJpgAndRaw = True"
-            ax.annotate(annot_text, xy=(0.05, 0.9), xycoords='axes fraction', ha='left', va='bottom', annotation_clip=False)
-            
+            ax.annotate(
+                annot_text,
+                xy=(0.05, 0.9),
+                xycoords="axes fraction",
+                ha="left",
+                va="bottom",
+                annotation_clip=False,
+            )
+
             ax.set_ylabel("# MasterRefIDs (samples)")
             ax.set_xlabel("Plant Type")
             # Add labels to each bar
             for bar_container in bar_plot.containers:
-                ax.bar_label(bar_container, label_type='edge', padding=3, fontsize=7)
+                ax.bar_label(bar_container, label_type="edge", padding=3, fontsize=7)
 
             fig.tight_layout()
             save_path = (
@@ -292,26 +373,29 @@ class BatchReport:
             fig, ax = plt.subplots(figsize=(12, 6))
 
             bar_plot = sns.barplot(
-                data=unique_ids_count,
-                x="UsState",
-                y="MasterRefID",
-                ax=ax,
-                width=0.25
+                data=unique_ids_count, x="UsState", y="MasterRefID", ax=ax, width=0.25
             )
-            
+
             ax.set_xticks(ax.get_xticks())
             ax.set_xticklabels(ax.get_xticklabels(), rotation=0)
             ax.figure.suptitle("Samples by State", fontsize=18)
 
-            # Adding a note under the title           
+            # Adding a note under the title
             annot_text = "$^{*}$HasMatchingJpgAndRaw = True"
-            ax.annotate(annot_text, xy=(0.05, 0.9), xycoords='axes fraction', ha='left', va='bottom', annotation_clip=False)
-            
+            ax.annotate(
+                annot_text,
+                xy=(0.05, 0.9),
+                xycoords="axes fraction",
+                ha="left",
+                va="bottom",
+                annotation_clip=False,
+            )
+
             ax.set_ylabel("# MasterRefIDs (samples)")
             ax.set_xlabel("State")
             # Add labels to each bar
             for bar_container in bar_plot.containers:
-                ax.bar_label(bar_container, label_type='edge', padding=3, fontsize=7)
+                ax.bar_label(bar_container, label_type="edge", padding=3, fontsize=7)
 
             fig.tight_layout()
             save_path = (
@@ -319,6 +403,7 @@ class BatchReport:
             )
             fig.savefig(save_path, dpi=300)
             log.info("Unique MasterRefIDs by UsState plot saved.")
+
 
 def main(cfg: DictConfig) -> None:
     """Main function to execute batch report tasks."""
@@ -331,5 +416,6 @@ def main(cfg: DictConfig) -> None:
 
     batchrep.plot_num_samples_season()
     batchrep.plot_num_samples_usstate()
+    batchrep.plot_sample_species_state_distribution()
     batchrep.num_uploads_last_7days_by_state()
     log.info(f"{cfg.general.task} completed.")
